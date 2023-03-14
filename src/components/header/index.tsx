6508d289b0777e06a9664c4814d65f33046f361b
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-14 19:31:54 -0300

    layout

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "./footer";
+import { Bottom } from "./bottom";
+import { Top } from "./top";
+////////////////////////////////
+export const Header = () => {
+  return (
+    <>
+      <Top /> 
+      <Bottom />
+      <Footer />
+    </>
+  );
+};
