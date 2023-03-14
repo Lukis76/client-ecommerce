0e28a359af281914d6423de41d5f618239ed8180
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-14 19:31:45 -0300

    top header layout

---
+++ src/components/header/top.tsx
@@ -0,0 +1,16 @@
+import css from "./styles/top.module.css";
+////////////////////////////////////////
+export const Top = () => {
+  return (
+    <section className={`${css.section}`}>
+      <div>
+        <span>free sping fre 100% libre de glutem</span>
+        <div>
+          <span>Hotline(0800-susana-te-estamos-llamando)</span>
+          <div>English</div>
+          <div>usd</div>
+        </div>
+      </div>
+    </section>
+  );
+};
