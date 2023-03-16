5d3d3df5b9ccaab890183696f3ed1486ee6db9c2
lucas manoguerra <kiu.manoguerra@gmail.com>
2023-03-15 21:10:33 -0300

    index component layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,13 +2,17 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../header";
 import { Footer } from "../footer";
-
+import css from './index.module.css'
+///////////////////////////////////////
 export const Layout = () => {
   return (
-    <div>
+    <main className={`${css.main}`}>
       <Header />
+      <section className={`${css.content}`}>
+
       <Outlet />
+      </section>
       <Footer />
-    </div>
+    </main>
   );
 };
