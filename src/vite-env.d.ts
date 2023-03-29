e9b99494569f4ca8cda202c008c42e34db7df557
Lucas.R.Manoguerra <kiu.manoguerra@gmail.com>
2023-03-29 19:31:53 -0300

    initial proyect

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="vite/client" />
+// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_BASE_URL: string
+  readonly VITE_BASE_URL_BACK: string
+  // más variables de entorno...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      ['@components']: path.resolve(__dirname, './src/components'),
+      ['@assets']: path.resolve(__dirname, './src/assets'),
+      ['@hooks']: path.resolve(__dirname, './src/hooks'),
+      ['@lib']:     path.resolve(__dirname, './src/lib'),
+      ['@utils']: path.resolve(__dirname, './src/utils'),
+      ['@types']: path.resolve(__dirname, './src/types'),
+      ['@api']: path.resolve(__dirname, './src/app/api'),
+      ['@app']: path.resolve(__dirname, './src/app'),
+      ['@pages']: path.resolve(__dirname, './src/pages'),
+    },
+  },
+  plugins: [
+    react(), 
+    tsconfigPaths()
+  ],
 })
+
